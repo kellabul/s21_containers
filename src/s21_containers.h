262f99f9ac5c8cac202a4f4a6c4ba5a08e3e5eee
Kella Bulghur <kellabul@oa-e6.msk.21-school.ru>
2023-03-05 19:36:28 +0300

    makefile + files for list created

---
+++ src/s21_containers.h
@@ -0,0 +1,9 @@
+#ifndef CPP2_S21_CONTAINERS_S21_CONTAINERS_H_
+#define CPP2_S21_CONTAINERS_S21_CONTAINERS_H_
+
+#include <iostream> // для удобства, в конце, возможно, удалим (если не забудем))
+
+#include "s21_list.h"
+
+
+#endif  // CPP2_S21_CONTAINERS_S21_CONTAINERS_H_
