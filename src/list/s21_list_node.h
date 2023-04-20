e6c30a30da4151f3c59c88370a61d299433fa3b1
Kella Bulghur <kellabul@student.21-school.ru>
2023-04-20 18:50:56 +0300

    list iterator operator* works correctly

--- src/list/s21_list_node.h
+++ src/list/s21_list_node.h
@@ -1,7 +1,5 @@
-#ifndef CPP2_S21_CONTAINERS_S21_LIST_NODE_H_
-#define CPP2_S21_CONTAINERS_S21_LIST_NODE_H_
-
-#include "s21_list.h"
+#ifndef CPP2_S21_CONTAINERS_S21_LIST_LIST_NODE_H_
+#define CPP2_S21_CONTAINERS_S21_LIST_LIST_NODE_H_
 
 namespace s21 {
 template <typename T>
@@ -12,12 +10,12 @@ class ListNode {
  public:
   ListNode() = default;
   explicit ListNode(const value_type value) : value_(value) {}
+  value_type &get_value() { return value_; }
 
- private:
   value_type value_ = 0;
   ListNode<T> *prev_ = nullptr;
   ListNode<T> *next_ = nullptr;
 };
 }  // namespace s21
 
-#endif  // CPP2_S21_CONTAINERS_S21_LIST_NODE_H_
+#endif  // CPP2_S21_CONTAINERS_S21_LIST_LIST_NODE_H_
