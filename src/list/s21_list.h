821a20f6d8c600d134e9e7ba6e4d5b4de87f6960
Kella Bulghur <kellabul@student.21-school.ru>
2023-04-25 15:38:48 +0300

    refactoring, end_node was added

--- src/list/s21_list.h
+++ src/list/s21_list.h
@@ -9,19 +9,22 @@ template <typename T>
 class list {
  public:
   using value_type = T;
+  using node_type = ListNode<T>;
   using reference = T &;
   using const_reference = const T &;
   using iterator = ListIterator<T>;
   // using const_iterator = const ListIterator;
-  using node_type = ListNode<T>;
   using size_type = unsigned long long;
 
  public:
-  list() : head_(nullptr), tail_(new node_type()), size_(0) {}
+  list() : end_node_(), tail_(&end_node_), size_(0U) {
+    tail_->next_ = tail_;
+    tail_->prev_ = tail_;
+  }
   // list(size_type n)	parameterized constructor, creates the list of size n
-  explicit list(size_type n) : size_(n) {
+  explicit list(size_type n) : list() {
     while (n--) push_back(value_type());
-  };
+  }
   // list(std::initializer_list<value_type> const &items)	initializer list
 
   // constructor, creates list initizialized using std::initializer_list
@@ -30,10 +33,10 @@ class list {
 
   // list(list &&l)	move constructor
 
-  ~list() {while (--size)
-  {
-    erase(begin());
-  }
+  ~list() {
+    while (--size_) {
+      erase(begin());
+    }
   }
 
   // operator=(list &&l)	assignment operator overload for moving object
@@ -43,9 +46,9 @@ class list {
   // const_reference back()	access the last element
 
   // returns an iterator to the beginning
-  iterator begin() { return iterator(head_); }
+  iterator begin() { return iterator(tail_->prev_); }
   // returns an iterator to the end
-  iterator end() { return iterator(nullptr); }
+  iterator end() { return iterator(tail_); }
 
   // bool empty()	checks whether the container is empty
   // size_type size()	returns the number of elements
@@ -55,28 +58,22 @@ class list {
 
   // inserts elements into concrete pos and returns the iterator that points to
   // the new element
-  // iterator insert(iterator pos, const_reference value) {
-  // }
+  iterator insert(iterator pos, const_reference value) {
+    ++size_;
+    return pos.AddNode(value);
+    ;
+  }
 
   // void erase(iterator pos)	erases element at pos
 
-void erase(iterator pos) {
-  if (pos != tail_)
-  pos.DeleteNode();
-  --size_;
-}
+  void erase(iterator pos) {
+    pos.DeleteNode();
+    --size_;
+  }
 
   // adds an element to the end
   void push_back(const_reference value) {
-    node_type *new_node = new ListNode(value);
-    if (!size_) {
-      head_ = new_node;
-    } else {
-      tail_->prev_->next_ = new_node;
-      new_node->prev_ = tail_->prev_;
-      new_node->next_ = tail_;
-      tail_->prev_ = new_node;
-    }
+    end().AddNode(value);
     ++size_;
   }
 
@@ -85,10 +82,15 @@ void erase(iterator pos) {
     erase(end());
   }
 
-  // void push_front(const_reference value)	adds an element to the head
-
-  // void pop_front()	removes the first element
-
+  // 	adds an element to the head
+void push_front(const_reference value) {
+    begin().AddNode(value);
+    ++size_;
+}
+  // 	removes the first element
+void pop_front() {
+    erase(begin());
+}
   // void swap(list& other)	swaps the contents
 
   // void merge(list& other)	merges two sorted lists
@@ -102,8 +104,8 @@ void erase(iterator pos) {
 
   // void sort()
 
-  node_type *head_;
-  node_type *tail_;  // stands after last element
+  node_type end_node_;
+  node_type *tail_;
   size_type size_;
 };
 }  // namespace s21
