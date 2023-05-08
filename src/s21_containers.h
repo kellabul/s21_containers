e193958ca27ecffe3034eae925d372e792776c94
Kella Bulghur <kellabul@su-e1.msk.21-school.ru>
2023-05-08 16:46:49 +0300

    files for map

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -4,7 +4,6 @@
 #include <iostream>  // для удобства, в конце, возможно, удалим (если не забудем))
 
 #include "list/s21_list.h"
-#include "list/s21_list_iterator.h"
-#include "list/s21_list_node.h"
+
 
 #endif  // CPP2_S21_CONTAINERS_S21_CONTAINERS_H_
