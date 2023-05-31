335517329047d03c3cc27fa20663b59362380450
Kella Bulghur <kellabul@student.21-school.ru>
2023-05-31 19:39:28 +0300

    rbtree iterator in progress

--- src/rb_tree/s21_RBTree_iterator.h
+++ src/rb_tree/s21_RBTree_iterator.h
@@ -1,17 +1,11 @@
 #ifndef CPP2_S21_CONTAINERS_S21_MAP_MAP_H_S21_RBTree_ITERATOR_H_
 #define CPP2_S21_CONTAINERS_S21_MAP_MAP_H_S21_RBTree_ITERATOR_H_
 
+#include "s21_RBTree_node.h"
+
 namespace s21 {
 template <typename Key>
-class RBTreeIterator {
- public:
-  using key_type = Key;
-  using node_type = RBTreeNode<key_type>;
-  using iterator = RBTreeIterator<Key>;
-  
- private:
-  node_type *node_pointer_;
-};
+
 }  // namespace s21
 
 #endif  // CPP2_S21_CONTAINERS_S21_MAP_MAP_H_S21_RBTree_ITERATOR_H_
