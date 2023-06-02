8096582d3ff48fa22a6b0baddafbc38cbc382cee
Kella Bulghur <kellabul@student.21-school.ru>
2023-06-02 14:37:16 +0300

    increased test coverage for rbtree

--- src/rb_tree/s21_RBTree_iterator.h
+++ src/rb_tree/s21_RBTree_iterator.h
@@ -13,7 +13,7 @@ class RBTreeIterator {
  public:
   using key_type = Key;
   using node_type = RBTreeNode<key_type>;
-  using iterator = RBTreeIterator;
+  using iterator = RBTreeIterator<key_type>;
   using rbtree = RBTree<key_type>;
 
  public:
@@ -71,7 +71,7 @@ class RBTreeIterator {
       node_ = nil_;
     } else {
       node_type *tmp = node_->parent_;
-      while (tmp != nil_ && node_ == tmp->right_) {
+      while (node_ == tmp->right_) {
         node_ = tmp;
         tmp = tmp->parent_;
       }
@@ -85,12 +85,12 @@ class RBTreeIterator {
       while (node_->right_ != nil_) {
         node_ = node_->right_;
       }
-      // nil_->right_ always points to max element
+      // nil_->right_ always points to min element
     } else if (node_ == nil_->right_) {
       node_ = nil_;
     } else {
       node_type *tmp = node_->parent_;
-      while (tmp != nil_ && node_ == tmp->left_) {
+      while (node_ == tmp->left_) {
         node_ = tmp;
         tmp = tmp->parent_;
       }
@@ -101,6 +101,103 @@ class RBTreeIterator {
  private:
   node_type *const nil_;
   node_type *node_;
+};
+
+
+
+template <typename Key>
+class RBTreeConstIterator {
+ public:
+  using key_type = Key;
+  using node_type = RBTreeNode<key_type>;
+  using iterator = RBTreeConstIterator<key_type>;
+  using rbtree = RBTree<key_type>;
+
+ public:
+  RBTreeConstIterator() = delete;
+
+  
+  explicit RBTreeConstIterator(rbtree *tree, node_type *node)
+      : nil_(tree->GetNil()), node_(node) {}
+
+  template <typename U>
+  explicit RBTreeConstIterator(const RBTreeConstIterator<U> &iter)
+      : nil_(iter.nil_), node_(iter.node_) {}
+
+  key_type operator*() const noexcept { return node_->key_; }
+
+  bool operator==(const iterator &other) const noexcept {
+    return node_ == other.node_;
+  }
+
+  bool operator!=(const iterator &other) const noexcept {
+    return node_ != other.node_;
+  }
+
+  void SetBegin() { node_ = nil_->right_; }
+  void SetEnd() { node_ = nil_; }
+
+  iterator &operator++() noexcept {
+    RBTreeIncrement();
+    return *this;
+  }
+  iterator &operator--() noexcept {
+    RBTreeDecrement();
+    return *this;
+  }
+
+  iterator operator++(int) noexcept {
+    iterator tmp{*this};
+    RBTreeIncrement();
+    return tmp;
+  }
+  iterator operator--(int) noexcept {
+    iterator tmp{*this};
+    RBTreeDecrement();
+    return tmp;
+  }
+
+  void RBTreeIncrement() {
+    if (node_->right_ != nil_) {
+      node_ = node_->right_;
+      while (node_->left_ != nil_) {
+        node_ = node_->left_;
+      }
+      // nil_->left_ always points to max element
+    } else if (node_ == nil_->left_) {
+      node_ = nil_;
+    } else {
+      node_type *tmp = node_->parent_;
+      while (node_ == tmp->right_) {
+        node_ = tmp;
+        tmp = tmp->parent_;
+      }
+      node_ = tmp;
+    }
+  }
+
+  void RBTreeDecrement() {
+    if (node_->left_ != nil_) {
+      node_ = node_->left_;
+      while (node_->right_ != nil_) {
+        node_ = node_->right_;
+      }
+      // nil_->right_ always points to min element
+    } else if (node_ == nil_->right_) {
+      node_ = nil_;
+    } else {
+      node_type *tmp = node_->parent_;
+      while (node_ == tmp->left_) {
+        node_ = tmp;
+        tmp = tmp->parent_;
+      }
+      node_ = tmp;
+    }
+  }
+
+ private:
+  node_type *const nil_;
+  node_type const *node_;
 };
 }  // namespace s21
 
